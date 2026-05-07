6c32f6452f8106bdb424e703f55017d682073d13
engMohamedWahba <90801164+engMohamedWahba@users.noreply.github.com>
2026-05-07 15:47:05 +0300

    Add files via upload
    Initial backend setup

---
+++ main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+import yt_dlp
+import uuid
+import os
+
+app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+DOWNLOAD_DIR = "downloads"
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+class DownloadRequest(BaseModel):
+    url: str
+
+@app.get("/")
+def root():
+    return {"status": "server running"}
+
+@app.post("/api/download")
+def download_video(data: DownloadRequest):
+
+    task_id = str(uuid.uuid4())
+    output_path = f"{DOWNLOAD_DIR}/{task_id}.mp4"
+
+    ydl_opts = {
+        "format": "best",
+        "outtmpl": output_path,
+        "noplaylist": True,
+        "quiet": True
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([data.url])
+
+        return {
+            "status": "ready",
+            "task_id": task_id,
+            "download_url": f"/file/{task_id}.mp4"
+        }
+
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
+
+@app.get("/file/{filename}")
+def get_file(filename: str):
+    return FileResponse(f"{DOWNLOAD_DIR}/{filename}")
